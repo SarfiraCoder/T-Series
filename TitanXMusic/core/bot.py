37cba31dd6acbfe45a0a76fcad75a93ad9161738
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 03:05:24 +0530

    Changed Log Text

--- TitanXMusic/core/bot.py
+++ TitanXMusic/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class Inflex(Client):
+class Titan(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="InflexMusic",
+            name="TitanMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
