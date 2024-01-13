56bfcc192f17cdb86e0b1439c4dccb3da402e836
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 17:58:09 +0530

    New Thumbnail

--- TitanXMusic/utils/thumbnails.py
+++ TitanXMusic/utils/thumbnails.py
@@ -9,7 +9,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
-from InflexMusic import app
+from TitanXMusic import app
 from config import YOUTUBE_IMG_URL
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -111,9 +111,9 @@ async def get_thumb(videoid):
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
     draw = ImageDraw.Draw(background)
-    arial = ImageFont.truetype("InflexMusic/assets/font2.ttf", 30)
-    font = ImageFont.truetype("InflexMusic/assets/font.ttf", 30)
-    title_font = ImageFont.truetype("InflexMusic/assets/font3.ttf", 45)
+    arial = ImageFont.truetype("TitanXMusic/assets/font2.ttf", 30)
+    font = ImageFont.truetype("TitanXMusic/assets/font.ttf", 30)
+    title_font = ImageFont.truetype("TitanXMusic/assets/font3.ttf", 45)
 
 
     circle_thumbnail = crop_center_circle(youtube, 400, 20)
@@ -153,7 +153,7 @@ async def get_thumb(videoid):
     draw.text((text_x_position, 400), "00:00", (255, 255, 255), font=arial)
     draw.text((1080, 400), duration, (255, 255, 255), font=arial)
 
-    play_icons = Image.open("InflexMusic/assets/play_icons.png")
+    play_icons = Image.open("TitanXMusic/assets/play_icons.png")
     play_icons = play_icons.resize((580, 62))
     background.paste(play_icons, (text_x_position, 450), play_icons)
 
