398dcc935d560c8805557fe1ae264310ce65c740
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 18:04:55 +0530

    __main__.py

--- TitanXMusic/__main__.py
+++ TitanXMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from InflexMusic import LOGGER, app, userbot
-from InflexMusic.core.call import Inflex
-from InflexMusic.misc import sudo
-from InflexMusic.plugins import ALL_MODULES
-from InflexMusic.utils.database import get_banned_users, get_gbanned
+from TitanXMusic import LOGGER, app, userbot
+from TitanXMusic.core.call import Titan
+from TitanXMusic.misc import sudo
+from TitanXMusic.plugins import ALL_MODULES
+from TitanXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,27 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("InflexMusic.plugins" + all_module)
-    LOGGER("InflexMusic.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("TitanXMusic.plugins" + all_module)
+    LOGGER("TitanXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Inflex.start()
+    await Titan.start()
     try:
-        await Inflex.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Titan.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("InflexMusic").error(
+        LOGGER("TitanXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
-    await Inflex.decorators()
-    LOGGER("InflexMusic").info(
-        "Inflex Music Bot Started Successfully"
+    await Titan.decorators()
+    LOGGER("TitanXMusic").info(
+        "Music Bot Started Successfully..."
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("InflexMusic").info("Stopping Inflex Music Bot...")
+    LOGGER("TitanXMusic").info("Stopping Music Bot...")
 
 
 if __name__ == "__main__":
