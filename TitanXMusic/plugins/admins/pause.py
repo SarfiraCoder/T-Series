ab0197b81dabe82283949ab7299363d8c0607f77
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 14:10:53 +0530

    Changed Line #15

--- TitanXMusic/plugins/admins/pause.py
+++ TitanXMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from InflexMusic import app
-from InflexMusic.core.call import Inflex
-from InflexMusic.utils.database import is_music_playing, music_off
-from InflexMusic.utils.decorators import AdminRightsCheck
-from InflexMusic.utils.inline import close_markup
+from TitanXMusic import app
+from TitanXMusic.core.call import Inflex
+from TitanXMusic.utils.database import is_music_playing, music_off
+from TitanXMusic.utils.decorators import AdminRightsCheck
+from TitanXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Inflex.pause_stream(chat_id)
+    await Titan.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
