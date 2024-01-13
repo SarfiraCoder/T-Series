0c7dca822101cb11aa4953f69c2f0e30f4c3f3f9
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 18:01:03 +0530

    __init__.py

--- TitanXMusic/__init__.py
+++ TitanXMusic/__init__.py
@@ -1,8 +1,8 @@
-from InflexMusic.core.bot import Inflex
-from InflexMusic.core.dir import dirr
-from InflexMusic.core.git import git
-from InflexMusic.core.userbot import Userbot
-from InflexMusic.misc import dbb, heroku
+from TitanXMusic.core.bot import Titan
+from TitanXMusic.core.dir import dirr
+from TitanXMusic.core.git import git
+from TitanXMusic.core.userbot import Userbot
+from TitanXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -11,7 +11,7 @@ git()
 dbb()
 heroku()
 
-app = Inflex()
+app = Titan()
 userbot = Userbot()
 
 
