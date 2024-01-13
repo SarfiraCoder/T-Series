c584ff9dbc8f32244a9adc8b35391a205f93ad96
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 14:24:13 +0530

    Changed Commands

--- TitanXMusic/plugins/admins/speed.py
+++ TitanXMusic/plugins/admins/speed.py
@@ -1,13 +1,13 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from InflexMusic import app
-from InflexMusic.core.call import Inflex
-from InflexMusic.misc import SUDOERS, db
-from InflexMusic.utils import AdminRightsCheck
-from InflexMusic.utils.database import is_active_chat, is_nonadmin_chat
-from InflexMusic.utils.decorators.language import languageCB
-from InflexMusic.utils.inline import close_markup, speed_markup
+from TitanXMusic import app
+from TitanXMusic.core.call import Titan
+from TitanXMusic.misc import SUDOERS, db
+from TitanXMusic.utils import AdminRightsCheck
+from TitanXMusic.utils.database import is_active_chat, is_nonadmin_chat
+from TitanXMusic.utils.decorators.language import languageCB
+from TitanXMusic.utils.inline import close_markup, speed_markup
 from config import BANNED_USERS, adminlist
 
 checker = []
@@ -94,7 +94,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await Inflex.speedup_stream(
+        await Titan.speedup_stream(
             chat_id,
             file_path,
             speed,
