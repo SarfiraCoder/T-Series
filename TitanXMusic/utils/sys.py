9e9adefc2de2e184abbbd858449fc1a4c933b0b2
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 17:50:53 +0530

    Sys Updatedd

--- TitanXMusic/utils/sys.py
+++ TitanXMusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from InflexMusic.misc import _boot_
-from InflexMusic.utils.formatters import get_readable_time
+from TitanXMusic.misc import _boot_
+from TitanXMusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
