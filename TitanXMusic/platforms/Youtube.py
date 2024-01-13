eca276c7515289564b4a286719b09ac6492633a6
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-13 13:58:51 +0530

    Changed API

--- TitanXMusic/platforms/Youtube.py
+++ TitanXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from InflexMusic.utils.database import is_on_off
-from InflexMusic.utils.formatters import time_to_seconds
+from TitanXMusic.utils.database import is_on_off
+from TitanXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
