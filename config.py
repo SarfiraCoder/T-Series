7f931959ac4d6be8c803b010a5b0db609eede1cd
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2025-07-17 19:23:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LostVenom/T-Series",
+    "https://github.com/SarfiraCoder/T-Series",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
